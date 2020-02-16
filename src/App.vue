86943f87b138a4a07cc891211a17b06214b03208
Mark Tilton <mark.tilton.a@gmail.com>
2020-02-15 22:10:34 -0500

    Add routing

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <div id="app">
-    <img id="logo" alt="logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="logo" alt="Vue logo" src="./assets/logo.png">
+    <div id="nav">
+      <router-link to="/">About Me</router-link> |
+      <router-link to="/about">Projects</router-link> |
+      <router-link to="/about">Contact</router-link> |
+      <router-link to="/about">Feeds</router-link>
+    </div>
+    <router-view id="content" />
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,11 +18,39 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  position: relative;
+  display: grid;
+  grid-template-rows: auto auto;
+  grid-template-columns: 100%;
 }
 
 #logo {
-  height: 128px;
-  width: 128px;  
+  grid-row: 1/2;
+  grid-column: 1/2;
+  width:128px;
+  height:128px;
 }
+
+#nav {
+  grid-row: 1 / 2;
+  grid-column: 1/2;
+  justify-self: right;
+  height: fit-content;
+  position:absolute;
+  top: 50%;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+#content {
+  grid-row: 2 / 3;
+}
+
 </style>
