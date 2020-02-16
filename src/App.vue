291b12a46a208dd760f8f70953dd0248fe044a8e
Mark Tilton <mark.tilton.a@gmail.com>
2020-02-16 15:41:35 -0500

    Update Style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <img id="logo" alt="Vue logo" src="./assets/logo.png">
+    <div id="title">
+      <img id="logo" alt="Mark Tilton" src="./assets/NegSpaceMTOrange.png" />
+    </div>
     <div id="nav">
-      <router-link to="/">About</router-link> |
-      <router-link to="/projects">Projects</router-link> |
-      <router-link to="/contact">Contact</router-link> |
-      <router-link to="/feeds">Feeds</router-link>
+      <router-link to="/">About </router-link>
+      <router-link to="/projects">Projects </router-link>
+      <router-link to="/contact">Contact </router-link>
+      <router-link to="/feeds">Feeds </router-link>
     </div>
     <router-view id="content" />
   </div>
@@ -13,48 +15,60 @@
 
 <style>
 #app {
+  margin: 20px;
+  padding: 0px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #dae4ee;
   position: relative;
   display: grid;
   grid-template-rows: auto auto;
   grid-template-columns: 100%;
 }
 
-#logo {
+#title {
   grid-row: 1/2;
   grid-column: 1/2;
-  width:128px;
-  height:128px;
+  justify-self: left;
+}
+
+#logo {
+  width: 128px;
+  height: 128px;
+  display: inline-block;
+}
+
+#name {
+  display: inline-block;
 }
 
 #nav {
   padding: 10px;
-  grid-row: 1 / 2;
+  grid-row: 1/2;
   grid-column: 1/2;
   justify-self: right;
   height: fit-content;
-  position:absolute;
+  position: absolute;
   top: 50%;
   -ms-transform: translateY(-50%);
   transform: translateY(-50%);
   margin: 0;
+  font-size: 18pt;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #dae4ee;
+  text-decoration: none;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #e65f23;
 }
 
 #content {
   grid-row: 2 / 3;
 }
-
 </style>
