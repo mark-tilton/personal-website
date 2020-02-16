27f1886ab58b4279179fc7f25fa7fc66594119c8
Mark Tilton <mark.tilton.a@gmail.com>
2020-02-16 15:41:24 -0500

    Add project component

---
+++ src/components/Project.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="Card">
+    <img id="thumbnail" :src="require(`../assets/${img}`)" />
+    <h3 id="header">
+      <a :href="link">{{ projectName }}</a>
+    </h3>
+    <p id="description">{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "project",
+  props: {
+    projectName: String,
+    link: String,
+    description: String,
+    img: String
+  }
+};
+</script>
+
+<style scoped>
+#Card {
+  padding: 0px;
+  margin: 10px 5px;
+  display: grid;
+  grid-template-rows: auto 100%;
+  grid-template-columns: 128px auto;
+  border: 1px;
+  border-color: #2a3536;
+  border-style: solid;
+  border-radius: 10px;
+}
+
+#header {
+  margin: 5px 10px;
+  font-weight: bold;
+  color: #dae4ee;
+  justify-self: left;
+  grid-row: 1 / 2;
+  grid-column: 2 / 3;
+  height: fit-content;
+}
+
+#header a {
+  padding: 0px;
+  text-decoration: none;
+}
+
+#header a:visited {
+  color: #dae4ee;
+}
+
+#description {
+  margin: 5px 10px;
+  grid-row: 2 / 3;
+  grid-column: 2 / 3;
+  justify-self: left;
+  text-align: left;
+}
+
+#thumbnail {
+  width: 128px;
+  height: 128px;
+  grid-row: 1 / 3;
+  grid-column: 1 / 2;
+  border-radius: 5px 0px 0px 5px;
+  padding: 0px;
+  margin: 0px;
+}
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,5 +1,51 @@
 <template>
   <div>
-    <h1>This is the projects page</h1>
+    <ul>
+      <li>
+        <Project
+          projectName="This website"
+          link="https://github.com/mark-tilton/personal-website"
+          description="A website made to highlight some of my personal projects."
+          img="vue_logo.png"
+        />
+        <Project
+          projectName="Quoridor"
+          link="https://github.com/mark-tilton/quoridor"
+          description="An implementation of the game Quoridor in C++.
+                 Planned Functionality
+Human player interaction
+Neural network players
+Ladder matchmaking server for bots
+Website to view games and statistics from ladder server"
+          img="quoridor_img.png"
+        />
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import Project from "@/components/Project.vue";
+
+export default {
+  name: "Projects",
+  components: {
+    Project
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+}
+a {
+  color: #42b983;
+}
+</style>
